1195ceca9cdcc91c97ded559feeb2b93a77a30ca
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2022-09-05 15:39:22 +0200

    update

--- sub.js
+++ sub.js
@@ -6,10 +6,11 @@ function randcolorgenerate() {
     var g = Math.round(Math.random() * 240 + 16)
     var b = Math.round(Math.random() * 240 + 16)
     var color = "#" + r.toString(16) + g.toString(16) + b.toString(16);
+    console.log(color);
     return color;
 }
 
 mainfocus.addEventListener('click', function onClick() {
     mainfocus.style.backgroundColor = randcolorgenerate();
-
+    mainfocus.style.color = randcolorgenerate();
 });
